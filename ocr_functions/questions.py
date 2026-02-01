b57a30402fa16acf256d2ea078d7199b2316dec0
Lucas Collins <lucasgcollins@outlook.com>
2026-02-01 14:34:23 +0000

    empty repo for ocr python

---
+++ ocr_functions/questions.py
@@ -0,0 +1,5 @@
+
+# Write an algorithm, using pseudocode or program code, to call the function push() with the 
+# value 15 and output a message saying "Added" if the value was successfully inserted onto the 
+# stack or "Not Added" if the stack is full.
+
